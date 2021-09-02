14f667fe2584a3416e53c5c340ec57ff3a1c46d7
Akane Shoji <cahierreco+dev@gmail.com>
2021-09-02 17:12:59 +0900

    Appバーの色変えてグリッドレイアウトの導入とか

--- cute-cats/src/Cat.tsx
+++ cute-cats/src/Cat.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+
+import Typography from '@material-ui/core/Typography'
+import { red } from '@material-ui/core/colors'
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
 import './App.css'
 
 type Props = {
@@ -10,6 +23,24 @@ type Props = {
 }
 
 function Cat(props: Props) {
+
+    const useStyles = makeStyles((theme: Theme) =>
+        createStyles({
+          root: {
+            maxWidth: 480,
+          },
+          media: {
+            height: 0,
+            paddingTop: '100%',
+          },
+          avatar: {
+            backgroundColor: red[500],
+          },
+        }),
+    )
+
+  const classes = useStyles()
+
     const text = props.text === undefined ? `%20` : props.text
     const fontSize = props.fontSize === undefined ? '50' : props.fontSize
     const color = props.color === undefined ? 'white' : props.color
@@ -18,8 +49,39 @@ function Cat(props: Props) {
   const url = `https://cataas.com/cat/says/${text}?s=${fontSize}&c=${color}&uniqueNum=${props.uniqueNum}`
 
   return (
-    <div className="App-body">
-        <LazyLoadImage src={url}/>
+    <div>
+      <Card className={classes.root}>
+        <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        title="Cute Cat"
+        subheader="September 2, 2021"
+      />
+      
+      <CardMedia
+        className={classes.media}
+        image={url}
+        title="cute cats"
+      />
+
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          めちゃくちゃかわいいねこ
+        </Typography>
+      </CardContent>
+        
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+      </Card>
     </div>
   )
 }
